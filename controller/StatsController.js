f347e4faa16a2186795acf116f01de3170618204
ashu <ashu@ashus-MacBook-Pro.local>
2024-06-23 22:02:04 -0700

    added room support

--- controller/StatsController.js
+++ controller/StatsController.js
@@ -1,5 +1,4 @@
 const catchAsync = require('../utils/catchAsync');
-const { v4 } = require('uuid');
 const ScreenModel = require('./../models/screenModel');
 
 class Queue {
@@ -43,179 +42,166 @@ class Queue {
 }
 
 const ephermal_db = {};
-const listeners = { "ash": [] };
+const redis_db = {};
+const listeners = {};
 const node_listener = {};
-ephermal_db["ash"] = new Queue();
-redis_db = { "ash": [] }
+
+function init_screen(screenID) {
+    listeners[screenID] = [];
+    ephermal_db[screenID] = new Queue();
+    redis_db[screenID] = [];
+}
+
+function create_random_screenID() {
+    let chars= "0123456789abcdefghiklmnopqrstuvwxyz";
+    let screenID = "";
+
+    for (let i = 0; i < 6; i++) {
+        let rnum = Math.floor(Math.random() * chars.length);
+        screenID += chars.substring(rnum, rnum + 1);
+    }
+    return screenID;
+}
 
 exports.create_screen = catchAsync(async (req, res) => {
-    let new_screen = v4();
+    let new_screen
+    while(true){
+        new_screen = create_random_screenID();
+        if(ephermal_db[new_screen] === undefined){
+            break;
+        }
+    }
 
-    ephermal_db[new_screen] = new Queue();
 
-    await ScreenModel.create({
-        screen_id: new_screen
-    });
-
-    return res.status(200).json({ screen_id: new_screen });
+    return res.status(200).json({ screenID: new_screen });
 });
 
 exports.get_screen = catchAsync(async (req, res) => {
-    let screen_id = req.body.screen_id;
-
-    if (ephermal_db[screen_id] === undefined) {
-        ephermal_db[screen_id] = new Queue();
-    }
-
-    let screen = await ScreenModel.findOne({ screen_id });
-    if (screen && screen.data) {
-        let initialData = JSON.parse(screen.data);
-        initialData.forEach(action => ephermal_db[screen_id].enqueue(action));
-    }
-
     return res.status(200).json({
         message: "screen retrieved",
-        data: ephermal_db[screen_id]
+        data: ephermal_db["dummy"].getDetails()
     });
 });
 
 exports.update_screen = catchAsync(async (req, res) => {
-    let screen_id = "ash";
-    let username = req.body.username;
-    let actions = req.body.actions;
+    const { screenID, username, actions } = req.body;
 
-    console.log(actions);
+    if (!ephermal_db[screenID]) {
+        return res.status(404).json({ message: "Screen not found" });
+    }
 
-    ephermal_db[screen_id].enqueue({ username, actions });
+    ephermal_db[screenID].enqueue({ username, actions });
+
+    // Optionally update the persistent database if needed here
 
     return res.status(200).json({
         message: "OK",
     });
 });
 
-exports.wait = (req, res) => {
-    let screenID = "ash";
-    let username = req.body.username;
+exports.wait = catchAsync(async (req, res) => {
+    const { screenID, username, initial } = req.body;
 
-    if (req.body.initial) {
-        ScreenModel.findOne({ screenID: screenID }).then(doc => {
-            return res.status(200).json({
-                message: "initial screen updated",
-                actions: JSON.parse(doc.data)
-            });
+    if (!screenID || !username) {
+        return res.status(400).json({ message: "screenID and username are required" });
+    }
 
-        })
-        return;
+    if (ephermal_db[screenID] === undefined) {
+        init_screen(screenID);
+    }
+
+    if (initial) {
+        const doc = await ScreenModel.findOneAndUpdate(
+            { screenID: screenID },
+            { $setOnInsert: { data: JSON.stringify([]) } },
+            { upsert: true, new: true }
+        );
+        return res.status(200).json({
+            message: "initial screen updated",
+            actions: JSON.parse(doc.data)
+        });
     }
 
     console.log("Added ", username, " to the listener list");
 
+    if (!listeners[screenID]) {
+        listeners[screenID] = [];
+    }
     listeners[screenID].push(username);
     node_listener[username] = res;
-};
+});
 
 exports.clear = catchAsync(async (req, res) => {
+    const { screenID } = req.body;
 
-    let screenID = "ash";
-    await ScreenModel.updateOne({ screenID }, { data: "[]" });
+    if (!screenID) {
+        return res.status(400).json({ message: "screenID is required" });
+    }
+
+    await ScreenModel.updateOne({ screenID: screenID }, { data: "[]" });
     redis_db[screenID] = [];
     ephermal_db[screenID] = new Queue();
     listeners[screenID] = [];
 
-
     return res.status(200).json({
         message: "screen cleared"
-    })
-
+    });
 });
 
 exports.snapshot = catchAsync(async (req, res) => {
-
-
-    //     let goes_to_db = [];
-    //     for (let event of redis_db["ash"]) {
-    //         console.log(event);
-    //         let datapoints = event[0].actions;
-    //     for (let j = 0; j < datapoints.length; j++) {
-    //         goes_to_db.push({ x: datapoints[j].x, y: datapoints[j].y });
-    //     }
-    //   }
-
-    //   let doc = await ScreenModel.findOne({ screenID: "ash" });
-    //   // parse the doc
-    //     let parsed_doc = JSON.parse(doc.data);
-    //     // add the new data
-    //     goes_to_db.forEach(point => {
-    //         parsed_doc.push(point);
-    //     });
-    //     // update the doc
-    //     await ScreenModel.updateOne({ screenID: "ash" }, { data: JSON.stringify(parsed_doc) });
-
-
-
-    //     return res.status(200).json({
-    //         message: "snapshot taken",
-    //         data: parsed_doc
-    //     });
-
-
+    // Implementation for snapshot
 });
 
-
 setInterval(async () => {
-    if (redis_db["ash"].length == 0) {
-        return
-    }
-    let goes_to_db = [];
-    for (let event of redis_db["ash"]) {
-        console.log(event);
-        let datapoints = event[0].actions;
-        for (let j = 0; j < datapoints.length; j++) {
-            goes_to_db.push({ x: datapoints[j].x, y: datapoints[j].y });
+    for (const screenID of Object.keys(redis_db)) {
+        if (redis_db[screenID].length === 0) {
+            continue;
         }
+
+        const goes_to_db = [];
+        for (const event of redis_db[screenID]) {
+            const datapoints = event[0].actions;
+            for (const datapoint of datapoints) {
+                goes_to_db.push({ x: datapoint.x, y: datapoint.y });
+            }
+        }
+
+        const doc = await ScreenModel.findOne({ screenID: screenID });
+        const parsed_doc = JSON.parse(doc.data || "[]");
+        goes_to_db.forEach(point => {
+            parsed_doc.push(point);
+        });
+        await ScreenModel.updateOne({ screenID: screenID }, { data: JSON.stringify(parsed_doc) });
+
+        redis_db[screenID] = [];
     }
-
-    let doc = await ScreenModel.findOne({ screenID: "ash" });
-    // parse the doc
-    let parsed_doc = JSON.parse(doc.data);
-    // add the new data
-    goes_to_db.forEach(point => {
-        parsed_doc.push(point);
-    });
-    // update the doc
-    await ScreenModel.updateOne({ screenID: "ash" }, { data: JSON.stringify(parsed_doc) });
-
-    redis_db["ash"] = [];
-
-
 }, 3000);
 
 setInterval(() => {
-    if (ephermal_db["ash"].isEmpty()) {
-        // console.log("No event for screen ash");
-        return;
-    }
-
-    let currentListeners = [...listeners["ash"]];
-
-    currentListeners.forEach(username => {
-        if (node_listener[username]) {
-            console.log("Sending response to ", username);
-
-            let next_wait_response = node_listener[username];
-            delete node_listener[username];
-            next_wait_response.status(200).json({
-                message: "screen updated",
-                actions: ephermal_db["ash"].getDetails()
-            });
-        } else {
-            console.log(`Listener ${username} not found`);
+    for (const screenID of Object.keys(ephermal_db)) {
+        if (ephermal_db[screenID] === undefined || ephermal_db[screenID].isEmpty()) {
+            continue;
         }
-    });
 
-    redis_db["ash"].push(ephermal_db["ash"].getDetails());
-    listeners["ash"] = [];
-    ephermal_db["ash"] = new Queue();
+        const currentListeners = [...listeners[screenID]];
+
+        currentListeners.forEach(username => {
+            if (node_listener[username]) {
+                console.log("Sending response to ", username);
+
+                const next_wait_response = node_listener[username];
+                delete node_listener[username];
+                next_wait_response.status(200).json({
+                    message: "screen updated",
+                    actions: ephermal_db[screenID].getDetails()
+                });
+            } else {
+                console.log(`Listener ${username} not found`);
+            }
+        });
+
+        redis_db[screenID].push(ephermal_db[screenID].getDetails());
+        listeners[screenID] = [];
+        ephermal_db[screenID] = new Queue();
+    }
 }, 100);
-
-
--- routes/publicRoute.js
+++ routes/publicRoute.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const statsController = require('../controller/StatsController');
 
-router.route('/screen').post(statsController.create_screen)
+router.route('/create').post(statsController.create_screen)
 router.route('/update/').post(statsController.update_screen)
 router.route('/wait/').post(statsController.wait)
 router.route('/snapshot/').post(statsController.snapshot)
